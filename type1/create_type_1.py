d0cec3e62eaa7e5892ad4d08d38f1bf51bb8a7fe
Caleb DeHaan <calebdehaan@gmail.com>
2019-11-18 00:37:50 -0600

    type1 works

--- type1/create_type_1.py
+++ type1/create_type_1.py
@@ -1,9 +1,13 @@
-import ast
-import pprint
+import os
+import random
 
 
 def create_type_1(filename):
-    tree = ast.parse(filename)
-    pprint.pprint(tree)
-
-    return tree
+    os.mkdir("/type1")
+    with open(filename) as fp:
+        with open(("type1/" + filename + random.randint(0, 100000)), 'w') as fw:
+            line = fp.readline()
+            random.randint(1, 5)
+            if random.randint(1, 5) == 1:
+                fw.write("\n")
+            fw.write(line)
