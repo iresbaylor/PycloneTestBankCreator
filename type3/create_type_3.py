58d4518eec2dbe38c060c20d0cb98abb9cc948e0
ST3PHANI3 <stephanie_alvord@baylor.edu>
2019-12-01 20:50:08 -0600

    modify BoolOp, ops, op

--- type3/create_type_3.py
+++ type3/create_type_3.py
@@ -1,41 +1,73 @@
-import ast
-import pprint
-from tokenize import tokenize, untokenize, NUMBER, STRING, NAME, OP
+import astor
+import random
 
 
 def create_type_3(filename):
-    # open file
-    file = open(filename, "r")
+    p = astor.code_to_ast.parse_file(filename)
+    modify_tree(astor.dump_tree(p))
 
-    # variables and stuff
-    found = 0
-    blank_line_count = 0
-    str = ""
 
-    nodes = []  # to hold all the ast for each function???
+def modify_tree(tree):
+    # find operators, switch them
+    # BoolOp(op=And, op=Or
+    # ops=[GtE], ops=[Eq], ops=[LtE], [Lt], [Gt], [Is], [IsNot], [NotEq]
+    # op=Add, Sub, Mult, Div, Mod
 
-    # parse to find def in file
-    for line in file:
-        index = line.find('def')
-        if index == 0 and found == 0:
-            # found a function definition
-            str = str+line
-            found = 1
-        elif found == 1:
-            # check if blank line (to determine end of function)
-            if line.isspace():
-                blank_line_count = blank_line_count+1
-            else:
-                blank_line_count = 0
-                # add to string of the whole function thing
-                str = str+line
+    lines = tree.split("\n")
+    indicators = ["BoolOp(op=", "ops=[", "op="]
+    opsOpt = ["GtE", "Eq", "LtE", "Lt", "Gt", "Is", "IsNot", "NotEq"]
+    opOpt = ["Add", "Sub", "Mult", "Div", "Mod"]
+    new_tree = ""
+    for l in lines:
+        c1 = l.count(indicators[0])
+        c2 = l.count(indicators[1])
+        c3 = l.count(indicators[2])
 
-        if blank_line_count == 2:
-            # finished with a function, make an ast of it
-            # print(str)
-            p = ast.parse(str)
-            print(ast.dump(p))
-            str = ""
-
-    # close file
-    file.close()
+        tLine = ""
+        if c1 > 0:
+            pieces = l.split(indicators[0])
+            tLine += pieces[0]
+            if l.count("And") > 0:
+                tLine += "Or,"
+            elif l.count("Or") > 0:
+                tLine += "And,"
+            # put rest of stuff back in
+            secondhalf = pieces[1].split(",")
+            i = 1
+            remain = len(secondhalf)
+            while remain > 1:
+                tLine += secondhalf[i]
+                i += 1
+                remain -= 1
+            new_tree += tLine+"\n"
+        elif c2 > 0:
+            firsthalf = l.split(indicators[1])
+            tLine += firsthalf[0]
+            secondhalf = l.split("]")
+            rnum = random.randrange(8)
+            tLine += "ops=[" + opsOpt[rnum] + "]"
+            # put the rest of the stuff back in
+            i = 1
+            remain = len(secondhalf)
+            while remain > 1:
+                tLine += secondhalf[i]
+                i += 1
+                remain -= 1
+            new_tree += tLine+"\n"
+        elif c3 > 0:
+            firsthalf = l.split(indicators[2])
+            tLine += firsthalf[0]
+            secondhalf = l.split(",")
+            rnum = random.randrange(5)
+            tLine += "op=" + opsOpt[rnum] + ","
+            # put the rest of the stuff back in
+            i = 1
+            remain = len(secondhalf)
+            while remain > 1:
+                tLine += secondhalf[i]
+                i += 1
+                remain -= 1
+            new_tree += tLine+"\n"
+        else:
+            new_tree += l+"\n"
+    print(new_tree)
