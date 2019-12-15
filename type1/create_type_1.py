0e9663b574718aaa7c036d8e29c995ed08f279e2
Caleb DeHaan <calebdehaan@gmail.com>
2019-12-14 18:40:33 -0600

    added type 1 random int variable

--- type1/create_type_1.py
+++ type1/create_type_1.py
@@ -5,7 +5,8 @@ import random
 def create_type_1(filename):
     os.mkdir("/type1")
     with open(filename) as fp:
-        with open(("type1/" + filename + random.randint(0, 100000)), 'w') as fw:
+        newfilename = os.path.splitext(filename[0])[0]
+        with open(("type1/" + newfilename + random.randint(0, 10000) + '.py'), 'w') as fw:
             line = fp.readline()
             line = line.strip()
             random.randint(1, 5)
