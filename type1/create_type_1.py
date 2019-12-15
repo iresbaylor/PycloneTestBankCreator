66a587edaa44e12174eccdfe50f44d3f6356d83c
Caleb DeHaan <calebdehaan@gmail.com>
2019-12-14 18:56:45 -0600

    finished with type 1

--- type1/create_type_1.py
+++ type1/create_type_1.py
@@ -3,14 +3,29 @@ import random
 
 
 def create_type_1(filename):
-    os.mkdir("/type1")
+    if not os.path.exists('type1clones'):
+        print('Creating type1clones directory...')
+        os.mkdir('type1clones')
     with open(filename) as fp:
-        newfilename = os.path.splitext(filename[0])[0]
-        with open(("type1/" + newfilename + random.randint(0, 10000) + '.py'), 'w') as fw:
+        print('original file name: ' + filename)
+        newfilename = os.path.splitext(filename)[0]
+        newfilename = 'type1clones/' + newfilename + str(random.randint(0, 10000)) + '.py'
+        with open((newfilename), 'w') as fw:
+            print('printing clone of ' + filename + ' to: type1clones/' + newfilename + '.py')
             line = fp.readline()
-            line = line.strip()
-            random.randint(1, 5)
-            if random.randint(1, 5) == 1:
-                fw.write("\n")
-            if not line.startswith('#'):
-                fw.write(line)
+            linenumber = 1
+            newlinenumber = 1
+            while line:
+                random.randint(1, 5)
+                if random.randint(1, 5) == 1:
+                    print('Adding a newline at line #: ' + str(newlinenumber))
+                    newlinenumber += 1
+                    fw.write("\n")
+                if not line.strip().startswith('#'):
+                    fw.write(line)
+                else:
+                    print('Stripping in-line comment line #: ' + str(linenumber))
+                line = fp.readline()
+                newlinenumber += 1
+                linenumber += 1
+            print('finished printing clone of ' + filename + ' to: type1clones/' + newfilename + '.py')
