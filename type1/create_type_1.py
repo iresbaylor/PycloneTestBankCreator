c3e33beeedda61deb7d39e0f91041ce5bedc96a1
Caleb DeHaan <calebdehaan@gmail.com>
2019-12-14 18:26:29 -0600

    added type 1 removal of comments

--- type1/create_type_1.py
+++ type1/create_type_1.py
@@ -7,7 +7,9 @@ def create_type_1(filename):
     with open(filename) as fp:
         with open(("type1/" + filename + random.randint(0, 100000)), 'w') as fw:
             line = fp.readline()
+            line = line.strip()
             random.randint(1, 5)
             if random.randint(1, 5) == 1:
                 fw.write("\n")
-            fw.write(line)
+            if not line.startswith('#'):
+                fw.write(line)
